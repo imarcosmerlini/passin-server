00847129c2d86ca06156f415d222a2d20cade86b
Marcos Merlini <merlinimarcos@gmail.com>
2024-04-06 13:13:42 -0300

    create routes to create events and register user to event

---
+++ src/utils/generate-slug.ts
@@ -0,0 +1,11 @@
+export function generateSlug(text: string): string {
+    return text
+        .toString()
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[^\w\s]/g, '')
+        .replace(/\s+/g, '-')
+        .replace(/^-+/g, '')
+        .replace(/-+$/g, '');
+}
